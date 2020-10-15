11526d02fd2b9f98375c409e06696c84d900c4ef
blacktunes <1719513828@qq.com>
2020-10-15 19:02:28 +0800

    增加阅读器demo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/style/transition.styl'
 
 createApp(App).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,123 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="jsx">
+import { ref, computed, Transition } from 'vue'
+import Epub from 'epubjs'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  setup () {
+    const readPercent = ref(0)
+    const readTime = ref(0)
+    // setInterval(() => {
+    //   readTime.value += 1
+    // }, 1000)
+    const readTimeStr = computed(() => {
+      return Math.ceil((readTime.value + 1) / 60)
+    })
+
+    const showMunu = ref(false)
+
+    const book = Epub('/test.epub')
+    const render = book.renderTo('book', { flow: 'paginated', width: '100%', height: '100%' })
+    render.display()
+
+    const prevPage = () => {
+      render.prev()
+      readPercent.value = getPercent(book.rendition.currentLocation().start.cfi)
+    }
+
+    const nextPage = () => {
+      render.next()
+      readPercent.value = getPercent(book.rendition.currentLocation().start.cfi)
+    }
+
+    const getPercent = (cfi) => {
+      return book.locations.percentageFromCfi(cfi).toFixed(2)
+    }
+
+    book.ready.then(() => {
+      book.locations.generate(750 * (window.innerWidth / 375))
+        .then(() => {
+          // render.display('Text/chapter11.html')
+        })
+    })
+
+    return () => (
+      <div class="book" id="book">
+        <div class="wrapper">
+          <div onClick={prevPage} class="left" />
+          <div onClick={() => { showMunu.value = true }} class="middle" />
+          <div onClick={nextPage} class="right" />
+          <Transition name="fade">
+            {
+              showMunu.value ? (
+                <div onClick={() => { showMunu.value = false }} class="mask"></div>
+              ) : null
+            }
+          </Transition>
+          <Transition name="slide-up">
+            {
+              showMunu.value ? (
+                <div class="menu">menu</div>
+              ) : null
+            }
+          </Transition>
+        </div>
+        <span class="read-time">已读: {readTimeStr.value}分钟</span>
+        <span class="read-percent">{readPercent.value}%</span>
+      </div>
+    )
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+::v-slotted(.book)
+  overflow hidden
+  position relative
+  height 100%
+  width 100%
+
+  .wrapper
+    display flex
+    z-index 1
+    position absolute
+    top 0
+    left 0
+    height 100%
+    width 100%
+
+    .mask
+      z-index 2
+      position absolute
+      top 0
+      right 0
+      height 100%
+      width 100%
+      background rgba(255, 255, 255, 0.5)
+
+    .menu
+      z-index 2
+      z-index 3
+      position absolute
+      bottom 0
+      left 0
+      height 150px
+      width 100%
+      background #666
+
+    div
+      flex 0 0 33.33%
+
+  .read-percent
+    position absolute
+    right 30px
+    bottom 5px
+    font-size 12px
+    color #666
+
+  .read-time
+    position absolute
+    left 15px
+    bottom 5px
+    font-size 12px
+    color #666
+</style>
